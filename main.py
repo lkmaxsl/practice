09a33f62ff5a39d0d3baafbf327d41fe6333d483
lkmaxsl <luchinamax@gmail.com>
2024-06-14 16:39:12 +0530

    16

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI,Query,Path,Body,Cookie,Header
-from typing import Optional, Literal
+from fastapi import FastAPI,Query,Path,Body,Cookie,Header,status,Form
+from typing import Optional, Literal,Union
 from pydantic import BaseModel, Field, HttpUrl, EmailStr
 from uuid import UUID
 from datetime import datetime, time, timedelta
@@ -220,4 +220,107 @@ def read_item_public_data(item_id: Literal['foo','bar','baz']):
     return items[item_id]
 
 Part 14 - Extra Models
-    '''
+   
+class UserBase(BaseModel):
+    username:str
+    password:str
+    email: EmailStr
+    full_name: str | None=None
+
+class UserIn(UserBase):
+    password:str
+
+class UserOut(UserBase):
+   pass
+
+class UserInDB(UserBase):
+    hashed_password:str
+  
+def fake_password_hasher(raw_password: str):
+    return f"supersecret{raw_password}"
+
+   
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("UserIn.dict", user_in.dict())
+    print("user saved")
+    print("user saved")
+    return user_in_db
+
+@app.post("/user",response_model=UserOut)
+def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
+
+class BaseItem(BaseModel):
+    description:str
+    type:str
+
+class CarItem(BaseItem):
+    type = "car"
+
+class PlaneItem(BaseItem):
+    type = "plane"
+    size: int
+
+items = {
+    "item1":{"description": "All my friends drive a low rider", "type": "car"},
+    "item2":{"description": "Music is my aeroplane, it's my aeroplane","type": "plane","size":5},
+}
+
+@app.get("/items/{item_id}",response_model=Union[PlaneItem,CarItem])
+def read_item(item_id: Literal["item1","item2"]):
+    return items[item_id]
+
+class ListItem(BaseModel):
+    name:str
+    description:str
+
+list_items = [
+    {"name":"Foo","description":"there comes my hero"},
+    {"name":"Red","description":"It's my aeroplane"}
+]
+
+app.get("/list_items",response_model=list[ListItem])
+def read_items():
+    return items
+
+app.get("/arbitrary",response_model=dict[str,float])
+def get_arbitrary():
+    return {"Foo":1,"bar":2}
+
+Part 15 - Response Status Codes
+
+@app.post("/items", status_code=201)
+def crate_item(name: str):
+    return {"name":name}
+
+@app.post("/items", status_code=status.HTTP_201_CREATED)
+def crate_item2(name: str):
+    return {"name":name}
+
+@app.delete("/items/{pk}", status_code=status.HTTP_204_NO_CONTENT)
+def delet_item(pk: str):
+    print("pk",pk)
+    return pk
+
+@app.get("/items", status_code=status.HTTP_302_FOUND)
+def read_items_redirect():
+    return {"hello":"world"}
+
+part 16- Form Fields
+'''
+@app.post("login")
+def login(username:str = Form(...),password:str=form(...)):
+    print("password",password)
+    return {"username": username}
+
+class User(BaseModel):
+    username:str
+    password:str
+
+@app.post ("/login-json")
+def login_json(username:str=Body(...), password: str=Body(...)):
+    print("password",password)
+    return {"username":username}
